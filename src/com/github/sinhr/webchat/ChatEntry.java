9dba174b854776dd6f48bdb71ba58774a82110c0
sinhr <lyudmyla.shapoval@gmail.com>
2013-03-02 23:57:27 +0200

    server

---
+++ src/com/github/sinhr/webchat/ChatEntry.java
@@ -0,0 +1,43 @@
+package com.github.sinhr.webchat;
+
+public class ChatEntry {
+	private String nick;
+	private String message;
+	
+	
+	/**
+	 * @param nick
+	 * @param inhalt
+	 */
+	public ChatEntry(String nick, String message) {
+		super();
+		this.nick = nick;
+		this.message = message;
+	}
+	/**
+	 * @return the nick
+	 */
+	public String getNick() {
+		return nick;
+	}
+	/**
+	 * @param nick the nick to set
+	 */
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param inhalt the inhalt to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
+}
---
+++ src/com/github/sinhr/webchat/ChatServer.java
@@ -0,0 +1,60 @@
+package com.github.sinhr.webchat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ChatServer
+ */
+@WebServlet("/ChatServer")
+public class ChatServer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	ArrayList<ChatEntry> chatroom;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ChatServer() {
+        super();
+        
+        chatroom = new ArrayList<ChatEntry>();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain");
+		
+		PrintWriter out = response.getWriter();
+		
+		for(ChatEntry ce : chatroom){
+			out.println(ce.getNick() + ": \t" + ce.getMessage() + "\n");
+		}
+		
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nick = request.getParameter("nickname");
+		String msg = request.getParameter("message");
+		
+		ChatEntry ce = new ChatEntry(nick, msg);
+		chatroom.add(ce);
+		System.out.println("adding");
+	}
+	
+	public void start() throws IOException {
+		// TODO
+	}
+}
